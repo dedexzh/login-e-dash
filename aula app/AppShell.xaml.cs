d7895c88d4ffae08b36dcb154d2850ad3ac9cbab
dedexzh <dedexcoding@gmail.com>
2025-12-11 09:18:40 -0300

    Adicionar arquivos de projeto.

---
+++ aula app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace aula_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ aula app/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace aula_app
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String Email = CampoEmail.Text;
+            String Senha = CampoSenha.Text;
+
+            if (Email == "dedexcoding@gmail.com" && Senha == "123456")
+            {
+                DisplayAlert("Login", "Login realizado com sucesso!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Login", "Email ou senha incorretos.", "Tentar novamente");
+            }
+
+        }
+    }
+}
